79b06eb6477d1a94d7fd0f9758b318cff94bdfe4
Fuzzywiskers <81891298+Fuzzywiskers@users.noreply.github.com>
2021-05-01 10:30:01 -0700

    Update 2.3

--- script.js
+++ script.js
@@ -26,3 +26,12 @@ function project_1(){
     console.log("testing 1")
     window.location="project-1-todo.html"
 }
+
+//side menu//
+function option1(){
+    document.getElementById("mySideMenu").style.width = "250px";
+}
+  
+function closeMenu(){
+    document.getElementById("mySideMenu").style.width = "0";
+}
