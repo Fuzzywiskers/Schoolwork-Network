32f8062516506fad0088d614da6e428ccd633e1d
Fuzzywiskers <81891298+Fuzzywiskers@users.noreply.github.com>
2021-05-01 16:34:08 -0700

    Update 2.5.3

---
+++ script3.js
@@ -0,0 +1,8 @@
+//side menu//
+function option1(){
+    document.getElementById("mySideMenu").style.width = "250px";
+}
+  
+function closeMenu(){
+    document.getElementById("mySideMenu").style.width = "0";
+}
