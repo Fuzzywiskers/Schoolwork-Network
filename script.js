1c1931e537242912b25ebf28776dd8f871a27641
Fuzzywiskers <81891298+Fuzzywiskers@users.noreply.github.com>
2021-05-02 09:34:21 -0700

    Update 2.3

--- script.js
+++ script.js
@@ -67,3 +67,20 @@ function createPro(){
     div.appendChild(div_d);
     document.getElementsByClassName("container-1")[0].appendChild(div);
 }
+
+//drop down menu//
+function openDrop(){
+    document.getElementById("drop-content").classList.toggle("show");
+}
+//close drop down//
+window.onclick = function(event){
+    if (!event.target.matches('.openbtn1')){
+        let dropdowns = document.getElementsByClassName("drop1-content");
+        for(i=0; i<dropdowns.length; i++){
+            let openDropdown = dropdowns[i];
+            if(openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show')
+            }
+        }
+    }
+}
